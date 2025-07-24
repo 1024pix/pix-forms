a54f32355d4d04c535304e50d056d196dc447401
Clément Latzarus <clement.latzarus@pix.fr>
2025-07-24 14:52:22 +0200

    feat: validate captcha response upon form submit
    Co-authored-by: Jérémie Jadé <jeremie.jade@pix.fr>

--- frontend/src/actions/index.ts
+++ frontend/src/actions/index.ts
@@ -4,27 +4,23 @@ import { FriendlyCaptchaClient } from "@friendlycaptcha/server-sdk";
 import 'dotenv/config';
 
 export const server = {
-	answer: defineAction({
-		input: z.object({
-			formId: z.string(),
-		}),
-		handler: async (input) => {
-			const { formId } = input;
-			// Handle the form submission logic here
-			console.log(`Form submitted with ID: ${formId}`);
-		},
-	}),
-	validateCaptcha: defineAction({
-		input: z.object({
-			captchaResponse: z.string(),
-		}),
-		handler: async ({captchaResponse}) => {
-			const frcClient = new FriendlyCaptchaClient({
-				apiKey: process.env.FRIENDLY_CAPTCHA_API_KEY,
-				sitekey: process.env.FRIENDLY_CAPTCHA_SITE_KEY,
-			});
-			const result = await frcClient.verifyCaptchaResponse(captchaResponse);
-			return result.shouldAccept()
-		}
-	})
+  answer: defineAction({
+    input: z.object({
+      formId: z.string(),
+      captchaResponse: z.string(),
+    }),
+    handler: async ({ formId, captchaResponse }) => {
+      const frcClient = new FriendlyCaptchaClient({
+        apiKey: process.env.FRIENDLY_CAPTCHA_API_KEY,
+        sitekey: process.env.FRIENDLY_CAPTCHA_SITE_KEY,
+      });
+      const result = await frcClient.verifyCaptchaResponse(captchaResponse);
+
+      if (result.shouldReject()) {
+        throw new Error("Captcha verification failed");
+      }
+
+      console.log(`Form submitted with ID: ${formId}`);
+    },
+  }),
 };
