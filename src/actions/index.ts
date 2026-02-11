d577d3c79b0c3955b76296474b5016016986ad51
P-Jeremy <jemyplu@gmail.com>
2026-02-11 11:10:01 +0100

    fix(types): resolve ts errors with nodemailer and friendlycaptcha

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -3,6 +3,7 @@ import { z } from "astro:schema";
 import { FriendlyCaptchaClient } from "@friendlycaptcha/server-sdk";
 import "dotenv/config";
 import nodemailer from "nodemailer";
+import type SMTPTransport from "nodemailer/lib/smtp-transport";
 import { processBase64Attachement } from "../services/attachements.ts";
 import getConfigParam from "../services/config.service.ts";
 import { createConversation } from "../services/freescout.service.ts";
@@ -35,17 +36,21 @@ export const server = {
 				"FRIENDLY_CAPTCHA_SITE_KEY",
 				true,
 			);
+
 			if (friendlyCaptchaSiteKey) {
 				const frcClient = new FriendlyCaptchaClient({
 					apiKey: getConfigParam("FRIENDLY_CAPTCHA_API_KEY", true),
 					sitekey: friendlyCaptchaSiteKey,
 				});
+
 				const result = await frcClient.verifyCaptchaResponse(captchaResponse);
 
-				if (result.shouldReject()) {
-					throw new Error(
-						`Le captcha n'a pas été validé (code erreur ${result.response?.error.detail}. Veuillez réessayer.`,
-					);
+				if (!result.shouldAccept()) {
+					if (result.response?.success === false) {
+						throw new Error(
+							`Le captcha n'a pas été validé (code erreur ${result.response?.error.detail}. Veuillez réessayer.`,
+						);
+					}
 				}
 			} else {
 				console.log(
@@ -65,15 +70,17 @@ export const server = {
 				const sender = `${formResult.customer_firstname} ${formResult.customer_lastname} <forms@pix.digital>`;
 				const subject = formResult.subject;
 
-				const transporter = nodemailer.createTransport({
+				const smtpOptions: SMTPTransport.Options = {
 					host: getConfigParam("SMTP_HOST"),
-					port: getConfigParam("SMTP_PORT"),
+					port: Number(getConfigParam("SMTP_PORT")),
 					secure: getConfigParam("SMTP_SECURE") === "true",
 					auth: {
 						user: getConfigParam("SMTP_USER"),
 						pass: getConfigParam("SMTP_PASS"),
 					},
-				});
+				};
+
+				const transporter = nodemailer.createTransport(smtpOptions);
 
 				const message = Object.entries(formResult)
 					.map(([key, value]) => {
