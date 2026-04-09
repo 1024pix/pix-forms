ac190dbadc7052134bddecb5ebe6ed2abf638804
Alexandre Monney <alexandre.monney77@gmail.com>
2026-04-09 17:20:17 +0200

    tech: add vitest for testing

---
+++ src/example.test.js
@@ -0,0 +1,8 @@
+import { expect, test } from "vitest";
+
+//TODO: A supprimer des que le premier vrai test sera écrit
+test("Example de test", async () => {
+ const example = true
+
+  expect(example).to.be.true
+});
