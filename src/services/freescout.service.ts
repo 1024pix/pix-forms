e4a260c184236ab96ec23327ba09e04cdbc14094
Thomas Bazin <thomas.bazin@pix.fr>
2026-05-11 18:27:32 +0200

    refactor: reinforce typing in freescout service

--- src/services/freescout.service.ts
+++ src/services/freescout.service.ts
@@ -2,163 +2,173 @@ import { processBase64Attachment } from "./attachments.service.ts";
 import { getConfigParam } from "./config.service.ts";
 
 type Attachement = {
-  name: string;
-  type: string;
-  content: string; // base64 encoded content
+	name: string;
+	type: string;
+	content: string; // base64 encoded content
 };
 
 type FormResult = {
-  customer_email: string;
-  customer_firstname: string;
-  customer_lastname: string;
-  subject: string;
-  message: string;
-  attachments: Attachement[];
-  [key: string]: string | number | Attachement[];
+	customer_email: string;
+	customer_firstname: string;
+	customer_lastname: string;
+	subject: string;
+	message: string;
+	attachments: Attachement[];
+	[key: string]: string | number | Attachement[];
+};
+
+type FreescoutAttachment = {
+	fileName: string;
+	mimeType: string;
+	data: string;
 };
 
 const getHeaders = () => ({
-  "X-FreeScout-API-Key": getConfigParam("FREESCOUT_API_KEY"),
-  "Content-Type": "application/json",
-  Accept: "application/json",
+	"X-FreeScout-API-Key": getConfigParam("FREESCOUT_API_KEY"),
+	"Content-Type": "application/json",
+	Accept: "application/json",
 });
 
 export async function createConversation(
-  formResult: FormResult,
-  freescoutMailboxId: number,
+	formResult: FormResult,
+	freescoutMailboxId: number,
 ) {
-  const url = new URL(
-    `${getConfigParam("FREESCOUT_API_URL")}/api/conversations`,
-  );
+	const url = new URL(
+		`${getConfigParam("FREESCOUT_API_URL")}/api/conversations`,
+	);
 
-  const body = {
-    type: "email",
-    mailboxId: freescoutMailboxId,
-    subject: formResult.subject,
-    customer: {
-      email: formResult.customer_email,
-      firstName: formResult.customer_firstname,
-      lastName: formResult.customer_lastname,
-    },
-    customFields: extractCustomFields(formResult, "custom_field_"),
-    threads: [
-      {
-        text: formResult.message,
-        type: "custom",
-        customer: {
-          email: formResult.customer_email,
-        },
-        attachments: formResult.attachments?.map(formatAttachment),
-      },
-    ],
-  };
+	const body = {
+		type: "email",
+		mailboxId: freescoutMailboxId,
+		subject: formResult.subject,
+		customer: {
+			email: formResult.customer_email,
+			firstName: formResult.customer_firstname,
+			lastName: formResult.customer_lastname,
+		},
+		customFields: extractCustomFields(formResult, "custom_field_"),
+		threads: [
+			{
+				text: formResult.message,
+				type: "custom",
+				customer: {
+					email: formResult.customer_email,
+				},
+				attachments: formResult.attachments?.map(formatAttachment),
+			},
+		],
+	};
 
-  const response = await fetch(url, {
-    method: "POST",
-    headers: getHeaders(),
-    body: JSON.stringify(body),
-  });
+	const response = await fetch(url, {
+		method: "POST",
+		headers: getHeaders(),
+		body: JSON.stringify(body),
+	});
 
-  const result = await response.json();
-  if (result.message === "Error occurred") {
-    throw new Error(JSON.stringify(result));
-  }
+	const result = await response.json();
+	if (result.message === "Error occurred") {
+		throw new Error(JSON.stringify(result));
+	}
 
-  console.log(`Conversation #${result.id} created`);
-  if (!result) return;
-  const { id: customerId, lastName, firstName } = result.customer;
-  await updateCustomerAndCustomerFields(
-    customerId,
-    firstName,
-    lastName,
-    formResult,
-  );
+	console.log(`Conversation #${result.id} created`);
+	if (!result) return;
+	const { id: customerId, lastName, firstName } = result.customer;
+	await updateCustomerAndCustomerFields(
+		customerId,
+		firstName,
+		lastName,
+		formResult,
+	);
 }
 
 async function updateCustomerAndCustomerFields(
-  customerId: number,
-  firstName: string,
-  lastName: string,
-  formResult: FormResult,
+	customerId: number,
+	firstName: string,
+	lastName: string,
+	formResult: FormResult,
 ) {
-  if (!customerId) {
-    console.log("pas de customer à update");
-    return;
-  }
-  await updateCustomer(customerId, firstName, lastName, formResult);
-  await updateCustomerFields(customerId, formResult);
+	if (!customerId) {
+		console.log("pas de customer à update");
+		return;
+	}
+	await updateCustomer(customerId, firstName, lastName, formResult);
+	await updateCustomerFields(customerId, formResult);
 }
 
 async function updateCustomer(
-  customerId: number,
-  firstName: string,
-  lastName: string,
-  formResult: FormResult,
+	customerId: number,
+	firstName: string,
+	lastName: string,
+	formResult: FormResult,
 ) {
-  if (
-    firstName === formResult.customer_firstname &&
-    lastName === formResult.customer_lastname
-  ) {
-    return;
-  }
-  const updateUrl = new URL(
-    `${getConfigParam("FREESCOUT_API_URL")}/api/customers/${customerId}`,
-  );
+	if (
+		firstName === formResult.customer_firstname &&
+		lastName === formResult.customer_lastname
+	) {
+		return;
+	}
+	const updateUrl = new URL(
+		`${getConfigParam("FREESCOUT_API_URL")}/api/customers/${customerId}`,
+	);
 
-  const body = JSON.stringify({
-    firstName: formResult.customer_firstname,
-    lastName: formResult.customer_lastname,
-  });
+	const body = JSON.stringify({
+		firstName: formResult.customer_firstname,
+		lastName: formResult.customer_lastname,
+	});
 
-  await fetch(updateUrl, {
-    method: "PUT",
-    headers: getHeaders(),
-    body,
-  });
+	await fetch(updateUrl, {
+		method: "PUT",
+		headers: getHeaders(),
+		body,
+	});
 }
 
 async function updateCustomerFields(
-  customerId: number,
-  formResult: FormResult,
+	customerId: number,
+	formResult: FormResult,
 ) {
-  const customerFields = extractCustomFields(formResult, "customer_field_");
+	const customerFields = extractCustomFields(formResult, "customer_field_");
 
-  if (!customerFields.length) {
-    console.log("pas de customerFields à update");
-    return;
-  }
+	if (!customerFields.length) {
+		console.log("pas de customerFields à update");
+		return;
+	}
 
-  const updateUrl = new URL(
-    `${getConfigParam("FREESCOUT_API_URL")}/api/customers/${customerId}/customer_fields`,
-  );
-  const body = JSON.stringify({ customerFields });
+	const updateUrl = new URL(
+		`${getConfigParam("FREESCOUT_API_URL")}/api/customers/${customerId}/customer_fields`,
+	);
+	const body = JSON.stringify({ customerFields });
 
-  await fetch(updateUrl, {
-    method: "PUT",
-    headers: getHeaders(),
-    body,
-  });
+	await fetch(updateUrl, {
+		method: "PUT",
+		headers: getHeaders(),
+		body,
+	});
 }
 
-export function formatAttachment({ name, type, content }: Attachement) {
-  return {
-    fileName: name,
-    mimeType: type,
-    data: processBase64Attachment(content),
-  };
+export function formatAttachment({
+	name,
+	type,
+	content,
+}: Attachement): FreescoutAttachment {
+	return {
+		fileName: name,
+		mimeType: type,
+		data: processBase64Attachment(content),
+	};
 }
 
 export function extractCustomFields(
-  formResult: FormResult,
-  customFieldPrefix: string,
-) {
-  return Object.entries(formResult)
-    .map(([key, value]) => {
-      if (key.startsWith(customFieldPrefix)) {
-        const customerFieldId = Number(key.replace(customFieldPrefix, ""));
-        return { id: customerFieldId, value };
-      }
-      return null;
-    })
-    .filter((field) => field !== null);
+	formResult: FormResult,
+	customFieldPrefix: string,
+): { id: number; value: string | number | Attachement[] }[] {
+	return Object.entries(formResult)
+		.map(([key, value]) => {
+			if (key.startsWith(customFieldPrefix)) {
+				const customerFieldId = Number(key.replace(customFieldPrefix, ""));
+				return { id: customerFieldId, value };
+			}
+			return null;
+		})
+		.filter((field) => field !== null);
 }
