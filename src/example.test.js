0785ac35884b977f1efc421266ba32f6719925b1
Thomas Bazin <thomas.bazin@pix.fr>
2026-05-12 11:06:02 +0200

    chore: delete example test

--- src/example.test.js
+++
@@ -1,8 +0,0 @@
-import { expect, test } from "vitest";
-
-//TODO: A supprimer des que le premier vrai test sera écrit
-test("Example de test", async () => {
-	const example = true;
-
-	expect(example).to.be.true;
-});
